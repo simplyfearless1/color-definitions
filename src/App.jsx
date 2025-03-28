b568494caa532af7009edb1ccbfef8cffdc51444
Ivan Nikolic <38662318+simplyfearless1@users.noreply.github.com>
2025-03-28 19:04:36 +0100

    Items List Table api request and local state

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
+import { useEffect, useState } from "react"
 import ItemsListTable from "./components/items-list-table"
 import PageHeader from "./components/page-header"
+import { request } from "./api"
+import { API_ENDPOINTS } from './config/api'
 
 const App = () => {
-  const itemsTable = [
-    {
-      title: 'Main',
-      hex: '#ffffff'
-    },
-    {
-      title: 'Second',
-      hex: '#000000'
-    },
-    {
-      title: 'Tringo',
-      hex: '#222222'
-    },   
-  ]
+  const [itemsTable, setItemsTable] = useState([{title: '', hex: ''}])
+  const handleDataSucess = async (res) => {
+    const req = await res
+    if(req.status === 200) {
+      setItemsTable([...req.data])
+    }
+  }
+  const fetchData = () => {
+    request(API_ENDPOINTS.COLORS, '', 'get', handleDataSucess)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+
 
   return (
     <>
