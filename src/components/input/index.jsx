9d803bccaf76bce0b70b001eb8256117eda7dd0a
Ivan Nikolic <38662318+simplyfearless1@users.noreply.github.com>
2025-03-29 03:09:11 +0100

    Add search functionality

--- src/components/input/index.jsx
+++ src/components/input/index.jsx
@@ -5,15 +5,17 @@ const Input = (
         id, 
         value, 
         label, 
-        handleChange 
+        handleChange,
+        classes,
+        type
     }
 ) => {
     return (
         <>
-            <label htmlFor={name} className="block text-sm/6 font-medium text-gray-900">{label}</label>
-            <div className="mt-2">
+            <label htmlFor={name} className='block text-sm/6 font-medium text-gray-900'>{label}</label>
+            <div className='mt-2'>
                 <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-blue-600">
-                    <input type="text" name={name} id={id} className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder={placeholder} value={value} onChange={handleChange} />
+                    <input type={type ? type : 'text'} name={name} id={id} className={`block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6 ${classes}`} placeholder={placeholder} value={value} onChange={handleChange} />
                 </div>
             </div>
         </>
--- src/components/items-list-table/index.jsx
+++ src/components/items-list-table/index.jsx
@@ -1,23 +1,38 @@
-import { useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import ItemEditModal from "../../components/item-edit-modal"
 import ItemRemoveModal from "../../components/item-remove-modal"
 import Spinner from '../spinner'
+import Input from '../input'
+import { ColorDefinitionsContext } from '../../context/colors'
 
-const ItemsListTable = ({ items, loading }) => {
-
+const ItemsListTable = () => {
+  const { colorDefinitions, loading } = useContext(ColorDefinitionsContext)
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
   const [id, setId] = useState(0)
   const [isEditModalVisible, setisEdiTModalVisible] = useState(false)
   const [isRemoveModalVisible, setisRemoveTModalVisible] = useState(false)
+  const handleInputChange = (e) => {
+    const val = e.target.value
+    setSearch(val)
+  }
   const handleItemEditClick = (id) => {
     setId(id)
     setisEdiTModalVisible(true)
   }
   const handleItemEditClose = () => setisEdiTModalVisible(false)
-  const handleItemRemoveClick = (id) => { 
+  const handleItemRemoveClick = (id) => {
     setId(id)
-    setisRemoveTModalVisible(true) 
+    setisRemoveTModalVisible(true)
   }
   const handleItemRemoveClose = () => setisRemoveTModalVisible(false)
+  const handleSearchResults = () => {
+    const reg = new RegExp(search, 'i')
+    setSearchResults(colorDefinitions.filter((item) => item.title.match(reg) || item.hex.match(reg)))
+  }
+  useEffect(() => {
+    handleSearchResults()
+  }, [search])
 
   return (
     <>
@@ -27,42 +42,75 @@ const ItemsListTable = ({ items, loading }) => {
         </div>
       ) : (
         <div className="max-w-7xl w-full mt-8 bg-white rounded-md p-8 shadow-lg">
-        <table className="table-auto w-full">
-          <thead>
-            <tr className="text-blue-600">
-              <th className="w-1/3">Color name</th>
-              <th className="w-1/3">Color hex</th>
-              <th className="w-1/3">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items?.length > 0 && items.map((item, index) => {
-              return (
-                <tr key={index} className="text-center">
-                  <td>
-                    {item.title}
-                  </td>
-                  <td>
-                    {item.hex}
-                  </td>
-                  <td>
-                    <button onClick={() => handleItemEditClick(item.id)}>
-                      <span className="text-4xl material-symbols-outlined">
-                        edit_document
-                      </span>
-                    </button>
-                    <button onClick={() => handleItemRemoveClick(item.id)}>
-                      <span className="material-symbols-outlined">
-                        delete
-                      </span>
-                    </button>
-                  </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      </div>
+          <div className='relative mx-auto max-w-xl mb-3'>
+            <span className="text-4xl material-symbols-outlined absolute bottom-1.5 left-1">
+              search
+            </span>
+            <Input placeholder='Search colors using color name or hex...' value={search} handleChange={handleInputChange} id='search' name='search' classes='pl-5' type='search' />
+          </div>
+          <table className="table-auto w-full">
+            <thead>
+              <tr className="text-blue-600">
+                <th className="w-1/3">Color name</th>
+                <th className="w-1/3">Color hex</th>
+                <th className="w-1/3">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {colorDefinitions?.length > 0 && search.length === 0 &&
+                colorDefinitions.map((item, index) => {
+                  return (
+                    <tr key={index} className="text-center">
+                      <td>
+                        {item.title}
+                      </td>
+                      <td>
+                        {item.hex}
+                      </td>
+                      <td>
+                        <button onClick={() => handleItemEditClick(item.id)}>
+                          <span className="text-4xl material-symbols-outlined">
+                            edit_document
+                          </span>
+                        </button>
+                        <button onClick={() => handleItemRemoveClick(item.id)}>
+                          <span className="material-symbols-outlined">
+                            delete
+                          </span>
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                })}
+              {search.length > 0 && (
+                searchResults.map((item, index) => {
+                  return (
+                    <tr key={index} className="text-center">
+                      <td>
+                        {item.title}
+                      </td>
+                      <td>
+                        {item.hex}
+                      </td>
+                      <td>
+                        <button onClick={() => handleItemEditClick(item.id)}>
+                          <span className="text-4xl material-symbols-outlined">
+                            edit_document
+                          </span>
+                        </button>
+                        <button onClick={() => handleItemRemoveClick(item.id)}>
+                          <span className="material-symbols-outlined">
+                            delete
+                          </span>
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                })
+              )}
+            </tbody>
+          </table>
+        </div>
       )}
       <ItemEditModal visible={isEditModalVisible} onClose={handleItemEditClose} onSubmit={handleItemEditClose} id={id} />
       <ItemRemoveModal visible={isRemoveModalVisible} onClose={handleItemRemoveClose} onSubmit={handleItemRemoveClose} id={id} />
