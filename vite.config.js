54bc1805cf90d7c27cbf082e4547afc4c8e2029a
Ivan Nikolic <38662318+simplyfearless1@users.noreply.github.com>
2025-03-29 03:39:45 +0100

    Add vite alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
+      components: `${path.resolve(__dirname, "./src/components/")}`,
+      config: `${path.resolve(__dirname, "./src/config/")}`,
+      api: path.resolve(__dirname, "./src/api"),
+      context: `${path.resolve(__dirname, "./src/context")}`,
+    },
+  },
+
 })
