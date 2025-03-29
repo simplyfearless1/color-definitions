39d220bf66771120c532e47c45b7a5482ad840a0
Ivan Nikolic <38662318+simplyfearless1@users.noreply.github.com>
2025-03-29 03:10:18 +0100

    Search (#7)
    * Add search functionality to Items list table

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useContext } from "react"
 import ItemsListTable from "./components/items-list-table"
 import PageHeader from "./components/page-header"
-import { ColorDefinitionsContext } from './context/colors'
 
 const App = () => {
-  const { colorDefinitions, loading } = useContext(ColorDefinitionsContext)
-
   return (
     <>
       <div className="flex flex-col justify-center items-center">
         <PageHeader />
-        <ItemsListTable items={colorDefinitions} loading={loading} />
+        <ItemsListTable />
       </div>
     </>
   )
